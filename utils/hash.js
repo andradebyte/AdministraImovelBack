dc429158ec6047a19fc98983fd8e3a758a117e66
andradebyte <joaoigor21052004@gmail.com>
2025-05-19 22:46:54 -0300

    +

--- utils/hash.js
+++ utils/hash.js
@@ -1,10 +1,21 @@
 import bcrypt from "bcrypt";
 
 export const hashPassword = async (password) => {
+
+    if (!password) {
+        throw new Error("Password is required");
+    }
+
     const salt = await bcrypt.genSalt(10);
-    return await bcrypt.hash(password, salt); // ✅
+    return await bcrypt.hash(password, salt);
 }
 
 export const isPasswordValid = async (password, hashedPassword) => {
+
+    if (!password || !hashedPassword) {
+        console.error("Senha ou hash não fornecidos:", { password, hashedPassword });
+        throw new Error("Senha e hash são obrigatórios para verificação.");
+    }
+
     return await bcrypt.compare(password, hashedPassword);
 }
